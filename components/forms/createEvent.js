c719d3d0a14e371498a08c5840936fd0f78fdbbd
marcfehr <marc.fehr@gmail.com>
2022-08-13 08:26:53 +0200

    add event form

--- components/forms/createEvent.js
+++ components/forms/createEvent.js
@@ -1,6 +1,21 @@
-import dynamic from 'next/dynamic'
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
-import 'easymde/dist/easymde.min.css'
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+
+const DatePickerComponent = () => {
+	const [startDate, setStartDate] = useState(new Date())
+	const DateTimeInput = ({ date, value, onChange }) => <input value={value} onChange={(e) => onChange(e.target.value)} />
+	return (
+		<DatePicker
+			selected={startDate}
+			onChange={(date) => setStartDate(date)}
+			showTimeSelect
+			timeFormat='p'
+			timeIntervals={15}
+			dateFormat='Pp'
+			customTimeInput={<DateTimeInput />}
+		/>
+	)
+}
 
 export default function EventForm({ onChangeHandler }) {
 	return (
@@ -11,20 +26,21 @@ export default function EventForm({ onChangeHandler }) {
 						<h3 className='text-lg font-medium leading-6 text-gray-900'>Calendar</h3>
 						<p className='mt-1 text-sm text-gray-500'>This is the information that will be synchronised with the Google Calendar API.</p>
 					</div>
+
 					<div className='mt-5 space-y-6 md:mt-0 md:col-span-2'>
 						<div className='grid grid-cols-3 gap-6'>
 							<div className='col-span-3 sm:col-span-2'>
 								<label htmlFor='company-website' className='block text-sm font-medium text-gray-700'>
 									Title
 								</label>
-								<div className='flex mt-1 rounded-md shadow-sm'>
+								<div className='flex mt-1'>
 									{/* <span className='inline-flex items-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50'>http://</span> */}
 									<input
 										onChange={onChangeHandler}
 										name='title'
 										// placeholder='Title'
 										// value={event.title}
-										className='w-full py-2 text-xl font-bold border-b-2 focus:outline-none'
+										className='w-full px-3 py-2 text-xl font-semibold border border-gray-300 rounded-md focus:outline-none'
 									/>
 								</div>
 							</div>
@@ -35,57 +51,63 @@ export default function EventForm({ onChangeHandler }) {
 								Description / notes
 							</label>
 							<div className='mt-1'>
-								<SimpleMDE
-									// value={event.content}
-									onChange={(value) => setEvent({ ...event, content: value })}
+								<textarea
+									id='about'
+									name='about'
+									rows={5}
+									className='block w-full border border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'
+									placeholder='you@example.com'
+									defaultValue={''}
 								/>
 							</div>
 						</div>
 
-						{/* <div>
-							<label className='block text-sm font-medium text-gray-700'>Photo</label>
-							<div className='flex items-center mt-1 space-x-5'>
-								<span className='inline-block w-12 h-12 overflow-hidden bg-gray-100 rounded-full'>
-									<svg className='w-full h-full text-gray-300' fill='currentColor' viewBox='0 0 24 24'>
-										<path d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z' />
-									</svg>
-								</span>
-								<button
-									type='button'
-									className='px-3 py-2 text-sm font-medium leading-4 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
-								>
-									Change
-								</button>
-							</div>
-						</div> */}
-
-						{/* <div>
-							<label className='block text-sm font-medium text-gray-700'>Cover photo</label>
-							<div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-								<div className='space-y-1 text-center'>
-									<svg className='w-12 h-12 mx-auto text-gray-400' stroke='currentColor' fill='none' viewBox='0 0 48 48' aria-hidden='true'>
-										<path
-											d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-											strokeWidth={2}
-											strokeLinecap='round'
-											strokeLinejoin='round'
-										/>
-									</svg>
-									<div className='flex text-sm text-gray-600'>
-										<label
-											htmlFor='file-upload'
-											className='relative font-medium text-yellow-600 bg-white rounded-md cursor-pointer hover:text-yellow-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-yellow-500'
-										>
-											<span>Upload a file</span>
-											<input id='file-upload' name='file-upload' type='file' className='sr-only' />
-										</label>
-										<p className='pl-1'>or drag and drop</p>
-									</div>
-									<p className='text-xs text-gray-500'>PNG, JPG, GIF up to 10MB</p>
+						<div className='mt-8 space-y-4'>
+							<div className='flex items-start'>
+								<div className='flex items-center h-5'>
+									<input id='comments' name='comments' type='checkbox' className='w-4 h-4 text-yellow-600 border-gray-300 rounded focus:ring-yellow-500' />
+								</div>
+								<div className='ml-3 text-sm'>
+									<label htmlFor='comments' className='font-medium text-gray-700'>
+										Full day event
+									</label>
+									<p className='text-gray-500'>Save event without time settings.</p>
 								</div>
 							</div>
-						</div> */}
-            
+							<div className='flex items-start'>
+								<div className='flex items-center h-5'>
+									<input id='comments' name='comments' type='checkbox' className='w-4 h-4 text-yellow-600 border-gray-300 rounded focus:ring-yellow-500' />
+								</div>
+								<div className='ml-3 text-sm'>
+									<label htmlFor='comments' className='font-medium text-gray-700'>
+										Multiple day event
+									</label>
+									<p className='text-gray-500'>Event does not start and end on the same day.</p>
+								</div>
+							</div>
+						</div>
+
+						<div className={'grid grid-cols-2'}>
+
+							<div>
+								<label htmlFor='about' className='block text-sm font-medium text-gray-700'>
+									From
+								</label>
+								<div className='mt-1'>
+									<DatePickerComponent />
+								</div>
+							</div>
+
+							<div>
+								<label htmlFor='about' className='block text-sm font-medium text-gray-700'>
+									To
+								</label>
+								<div className='mt-1'>
+									<DatePickerComponent />
+								</div>
+							</div>
+							
+						</div>
 					</div>
 				</div>
 			</div>
