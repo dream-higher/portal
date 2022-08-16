ccf782a259457eb8cbb937784bd28253ba1e9396
mf^intel <marc.fehr@gmail.com>
2022-08-16 08:30:19 +0200

    changing auth lib

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import { Toaster } from 'react-hot-toast'
 import { UserProvider } from '@supabase/auth-helpers-react'
 import { supabaseClient } from '@supabase/auth-helpers-nextjs'
 
+import { SessionProvider } from 'next-auth/react'
+
 import '@styles/tailwind.css'
 import '@styles/global.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
 	return (
-		<UserProvider supabaseClient={supabaseClient}>
-			<Component {...pageProps} />
-			<Toaster />
-		</UserProvider>
+		<SessionProvider session={session}>
+			<UserProvider supabaseClient={supabaseClient}>
+				<Component {...pageProps} />
+				<Toaster />
+			</UserProvider>
+		</SessionProvider>
 	)
 }
--- pages/login.js
+++ pages/login.js
@@ -4,31 +4,38 @@ import { Auth } from '@supabase/ui'
 import { useUser } from '@supabase/auth-helpers-react'
 import { supabaseClient } from '@supabase/auth-helpers-nextjs'
 
+import LoginButton from '@components/google/login-button'
+
 const LoginPage = () => {
 	const { user, error } = useUser()
 
 	const router = useRouter()
 
-	if (!user) {
-		return (
-			<div className={'w-full h-full absolute top-0 left-0 flex justify-center items-center'}>
-				<div>
-					{error && <p>{error.message}</p>}
-					<Auth
-						supabaseClient={supabaseClient}
-						onlyThirdPartyProviders
-						providers={['google']}
-						socialLayout='horizontal'
-						socialButtonSize={'xlarge'}
-						redirectTo={process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}
-					/>
-				</div>
-			</div>
-		)
-	} else {
-		router.push(process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL)
-		return null
-	}
+	// if (!user) {
+	// 	return (
+	// 		<div className={'w-full h-full absolute top-0 left-0 flex justify-center items-center'}>
+	// 			<div>
+	// 				{error && <p>{error.message}</p>}
+	// 				<Auth
+	// 					supabaseClient={supabaseClient}
+	// 					onlyThirdPartyProviders
+	// 					providers={['google']}
+	// 					socialLayout='horizontal'
+	// 					socialButtonSize={'xlarge'}
+	// 					redirectTo={process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}
+	// 				/>
+	// 			</div>
+	// 		</div>
+	// 	)
+	// } else {
+	// 	router.push(process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL)
+	// 	return null
+	// }
+	return (
+		<div className={'w-full h-full absolute top-0 left-0 flex justify-center items-center'}>
+			<LoginButton />
+		</div>
+	)
 }
 
 export default LoginPage
