08a63145f4f4aad4a9ff1c2b59dbf122abf0d911
marcfehr <marc.fehr@gmail.com>
2022-08-16 08:30:53 +0200

    php changes m1

--- php/src/get-users.php
+++ php/src/get-users.php
@@ -1,4 +1,8 @@
 <?
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 require __DIR__ . '/vendor/autoload.php';
 
 // if (php_sapi_name() != 'cli') {
